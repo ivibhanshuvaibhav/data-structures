6c78dbfb6810dcf1fe9f7ec3775ca2d5a6bb4d90
Vibhanshu Vaibhav <vibhanshu_chaudhary@yahoo.co.in>
2017-04-08 00:45:48 +0530

    Sorting

--- sorting/BubbleSort.java
+++ sorting/BubbleSort.java
@@ -1,7 +1,30 @@
 package sorting;
 
-/**
- * Created by Vibhanshu on 19/03/17.
- */
 public class BubbleSort {
+
+    public static int[] bubbleSort(int[] array){
+
+        for (int i = 0; i < array.length - 1; i++){
+            for (int j = 0; j < array.length - 1; j++){
+                if (array[j] > array[j+1]){
+                    int temp = array[j];
+                    array[j] = array[j+1];
+                    array[j+1] = temp;
+                }
+            }
+        }
+
+        return array;
+    }
+
+    public static void main(String[] args) {
+
+        int[] array = {4,2,5,7,1,3,9,6};
+
+        for (int i: bubbleSort(array)){
+            System.out.println(i);
+        }
+
+    }
+
 }
--- sorting/QuickSort.java
+++ sorting/QuickSort.java
@@ -1,7 +1,51 @@
 package sorting;
 
-/**
- * Created by Vibhanshu on 19/03/17.
- */
 public class QuickSort {
+
+    public static int partition(int[] array, int left, int right){
+
+        int i = left, j = right;
+        int temp;
+        int pivot = array[(left+right)/2];
+
+        while (i <= j){
+            while (array[i] < pivot)
+                i++;
+            while (array[j] > pivot)
+                j--;
+            if (i <= j){
+                temp = array[i];
+                array[i] = array[j];
+                array[j] = temp;
+                i++;
+                j--;
+            }
+        }
+
+        return i;
+    }
+
+    public static void quickSort(int[] array, int left, int right){
+
+        int index = partition(array, left, right);
+        if (left < index - 1)
+            quickSort(array, left, index - 1);
+        if (index < right)
+            quickSort(array, index, right);
+
+    }
+
+    public static void main(String[] args) {
+
+        int[] array = {4,2,5,7,1,3,9,6};
+
+        quickSort(array, 0, array.length - 1);
+
+        for (int i: array){
+            System.out.println(i);
+        }
+
+
+    }
+
 }
